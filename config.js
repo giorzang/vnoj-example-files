c4e742bd552883cca8de323a73256b722614be7a
Vũ Trường Giang <84860408+giorzang@users.noreply.github.com>
2024-07-19 05:00:40 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  get_host: '192.168.1.100',
+  get_port: 15100,
+  post_host: '192.168.1.100',
+  post_port: 15101,
+  http_host: '192.168.1.100',
+  http_port: 15102,
+  long_poll_timeout: 29000
+};
+
+export default config;
